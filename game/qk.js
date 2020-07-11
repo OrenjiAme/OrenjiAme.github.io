0a4479462ef881bc2663cedef9e9e3502a9349b9
orenjiame <orenjiame@gmail.com>
2020-07-11 13:02:06 +0900

    クイズノック用

---
+++ game/qk.js
@@ -0,0 +1,9 @@
+function check(){
+    var i = document.getElementById("q1").value;
+    console.log(i);
+    if(i == "通分"){
+        document.open();
+        document.write("せいかい！");
+        document.close();
+    }
+}
