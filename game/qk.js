bf5eee63211254645985d1d94ad457b38df4a8da
orenjiame <orenjiame@gmail.com>
2020-07-12 15:18:43 +0900

    a

--- game/qk.js
+++
@@ -1,9 +0,0 @@
-function check(){
-    var i = document.getElementById("q1").value;
-    console.log(i);
-    if(i == "通分"){
-        document.open();
-        document.write("せいかい！");
-        document.close();
-    }
-}
