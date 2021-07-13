839966ba6dc7cbeabe0a648d4335aacb4ca46e0c
orenjiame <orenjiame@gmail.com>
2021-07-13 21:30:25 +0900

    a

---
+++ calender/calender.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById("canvas");        
+let imagePath = "8.png";
+draw(canvas,imagePath);
+function draw(canvas,imagePath){
+    console.log("draw");
+    const image = new Image();
+    image.addEventListener("load",function (){
+        canvas.width = image.naturalWidth;
+        canvas.height = image.naturalHeight;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(image, 0, 0);
+        console.log("load!");
+    });
+    image.src = imagePath;
+}
