69a541ed5f42972f12dac36b331680daeab4ecd3
orenjiame <orenjiame@gmail.com>
2021-07-13 22:19:40 +0900

    a

--- calender/calender.js
+++ calender/calender.js
@@ -1,15 +1,31 @@
-const canvas = document.getElementById("canvas");        
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d'); 
+let x = 0;
+let y = 0;
+
 let imagePath = "8.png";
 draw(canvas,imagePath);
+
+canvas.addEventListener('click', onClick, false);
+
+function onClick(e) {
+    var rect = e.target.getBoundingClientRect();
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+    var str = document.getElementById("name").value;
+    console.log(str);
+    ctx.font = "20px serif";
+    ctx.fillText(str,x,y);
+}
+
 function draw(canvas,imagePath){
-    console.log("draw");
     const image = new Image();
     image.addEventListener("load",function (){
         canvas.width = image.naturalWidth;
         canvas.height = image.naturalHeight;
         const ctx = canvas.getContext("2d");
         ctx.drawImage(image, 0, 0);
-        console.log("load!");
     });
     image.src = imagePath;
 }
+
