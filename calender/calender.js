659ad5302506b54bb62d4e65b128568824f5c48e
orenjiame <orenjiame@gmail.com>
2021-10-06 10:27:58 +0900

    a

--- calender/calender.js
+++ calender/calender.js
@@ -5,7 +5,7 @@ let x = 0;
 let y = 0;
 var bgColor = "rgb(0,0,0)";
 ctx.fillStyle = bgColor;
-let imagePath = "10to12.png";
+let imagePath = "12.png";
 var hist = [];
 var cnt = 0;
 
@@ -18,7 +18,6 @@ function onClick(e) {
     x = e.clientX - rect.left;
     y = e.clientY - rect.top;
     var str = document.getElementById("name").value;
-    console.log(str);
     ctx.font = "20px serif";
     ctx.fillText(str,x,y);
 }
@@ -46,43 +45,3 @@ $("#download").click(function(){
     document.getElementById("download").href = base64;
   });
 
-// スタックしておく最大回数。キャンバスの大きさの都合などに合わせて調整したら良いです。
-const STACK_MAX_SIZE = 5;
-// スタックデータ保存用の配列
-let undoDataStack = [];
-let redoDataStack = [];
-
-// canvasへの描画処理を行う前に行う処理
-function beforeDraw() {
-    // やり直し用スタックの中身を削除
-    redoDataStack = [];
-    // 元に戻す用の配列が最大保持数より大きくなっているかどうか
-    if (undoDataStack.length >= STACK_MAX_SIZE) {
-        // 条件に該当する場合末尾の要素を削除
-        undoDataStack.pop();
-    }
-    // 元に戻す配列の先頭にcontextのImageDataを保持する
-    undoDataStack.unshift(ctx.getImageData(0, 0, $canvas[0].width(), $canvas[0].height()));
-}
-
-function undo () {
-     // 戻す配列にスタックしているデータがなければ処理を終了する
-    if (undoDataStack.length <= 0) return;
-    // やり直し用の配列に元に戻す操作をする前のCanvasの状態をスタックしておく
-    redoDataStack.unshift(ctx.getImageData(0, 0, $canvas[0].width(), $canvas[0].height()));
-    // 元に戻す配列の先頭からイメージデータを取得して
-    var imageData = undoDataStack.shift();
-    // 描画する
-    ctx.putImageData(imageData, 0, 0);
-}
-
-function redo () {
-    // やり直し用配列にスタックしているデータがなければ処理を終了する
-    if (redoDataStack.length <= 0) return;
-    // 元に戻す用の配列にやり直し操作をする前のCanvasの状態をスタックしておく
-    undoDataStack.unshift(ctx.getImageData(0, 0, $canvas[0].width(), $canvas[0].height()));
-    // やり直す配列の先頭からイメージデータを取得して
-    var imageData = redoDataStack.shift();
-    // 描画する
-    ctx.putImageData(imageData, 0, 0);
-}
